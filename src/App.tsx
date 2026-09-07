cd37d7354c1565b9cd57d0f24b379f5c7c0951b4
Pharniyi <faniyitimi@gmail.com>
2026-09-07 15:41:57 +0100

    First commit: Portfolio setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <main className="min-h-screen bg-[#07050A] text-white">
+      <h1 className="p-10 text-4xl font-bold">
+        Timilehin Faniyi
+      </h1>
+    </main>
+    </>
+  )
+}
+
+export default App
